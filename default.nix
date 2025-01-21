c5c77800f740086385a313f326df6e9bbee0da94
Malik <abdelmalik.najhi@stud.hs-kempten.de>
2025-01-21 12:33:23 +0100

    Bump: hspec-core

--- default.nix
+++ default.nix
@@ -18,7 +18,7 @@
     formatting = haskellPkg "formatting";
     Glob = haskellPkg "Glob";
     hspec = haskellPkg "hspec";
-    hspec-core = haskellPkg "hspec-core_2_7_10";
+    hspec-core = haskellPkg "hspec-core";
     HUnit = haskellPkg "HUnit";
     language-javascript = haskellPkg "language-javascript";
     mtl = haskellPkg "mtl_2_3_1";
