199b7a49b80c690ccdae8ee07536b8056963974a
Malik <abdelmalik.najhi@stud.hs-kempten.de>
2025-02-01 11:47:57 +0100

    Bump: Base

--- default.nix
+++ default.nix
@@ -9,7 +9,7 @@
     ansi-terminal = haskellPkg "ansi-terminal";
     ansi-wl-pprint = haskellPkg "ansi-wl-pprint_0_6_9";
     async = haskellPkg "async";
-    base = haskellPkg "base_4_20_0_1";
+    base = haskellPkg "base_4_21_0_0";
     boxes = haskellPkg "boxes";
     bytestring = haskellPkg "bytestring_0_12_1_0";
     containers = haskellPkg "containers_0_7";
